d12fb0c9891a6cd09ba24663ad4a2def2c4d29e6
asmwarrior <asmwarrior@321e73ed-9b96-f238-7830-de6a307d5b6f>
2010-11-16 05:29:56 +0000

    skip cpp comment

---
+++ test_macro.cpp
@@ -0,0 +1,41 @@
+
+//Testing #define
+
+#define AAA (a,b)  function(a,b) // comment here
+
+#define BBB (a,b)  function(a,b) // comment here \
+comment continued
+
+#define BBB (a,b)  function1(a,b);\
+ function2(a,b)\
+ // comment here \
+comment continued
+
+
+//Testing string 
+
+"aaaa\\\"bbbb" 
+
+" aaa bbb \t ccc ddd \\  ff \
+jjj jijih88 999898"
+
+" aaa\\\""
+
+" aaa\\\"     aaa bbb \
+sssss dddefef \\  \t  \\ a\t \
+"
+
+
+ // aaa   \     \\
+ ccc ddd eee;\
+      
+ 
+ 
+   // aaa \\  shisfhidfsih\   s\\ \ \ \ ddd  \\ \   
+ ccc ddd eee;
+ 
+ //    sdfsfs\ dddd\ d
+ float c;
+ int a;
+ 
+ 
