b3ead66dfdcbd13d1224145e6628132000e8c8af
asmwarrior <asmwarrior@321e73ed-9b96-f238-7830-de6a307d5b6f>
2010-12-20 08:36:01 +0000

    from quex 0.57.1's new feature "source packager", I can bundle the quex base. Many thanks to Frank (author of Quex). see the related discussion here: http://sourceforge.net/projects/quex/forums/forum/574343/topic/4001340 Now, you can build the whole parser test project without installing Quex.

--- cppparser/cpp.bat
+++ cppparser/cpp.bat
@@ -1,4 +1,4 @@
 set d=%date:~0,10%
 set t=%time:~0,8%
 echo %d% %t% >>result.txt
-quex -i cpp.qx --engine tiny_lexer --buffer-based --token-policy single --token-memory-management-by-user --token-id-offset 2 --token-prefix TKN_  >> result.txt
+quex -i cpp.qx --engine tiny_lexer --buffer-based --token-policy single --token-memory-management-by-user --token-id-offset 2 --token-prefix TKN_  --source-package   lexer >> result.txt
