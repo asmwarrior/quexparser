021962fde96a3c00c8ad0a241e364721f12726f6
asmwarrior <asmwarrior@321e73ed-9b96-f238-7830-de6a307d5b6f>
2011-01-17 03:55:31 +0000

    clang test demo

---
+++ clang/test.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+
+
+
+class AAA
+{
+public:
+   int aaa;
+   int bbb;
+};
+
+
+int myfunction()
+{
+  AAA a;
+  //a. 
+  std::vector<AAA> b;
+  b[1].
+  return 0;
+}
